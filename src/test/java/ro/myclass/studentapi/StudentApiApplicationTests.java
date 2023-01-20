ac52a31c434f0cdccf86498503d07c2bd28d5166
Balanescu Raul <balanescuraul@gmail.com>
2023-01-20 20:23:56 +0200

    add student-api

---
+++ src/test/java/ro/myclass/studentapi/StudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.myclass.studentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
